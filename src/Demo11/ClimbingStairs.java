b31dd219b5fa724f2a9b0ee1233fb4730dceb10e
mercy <mercy.talamala@gmail.com>
2026-09-18 15:45:19 +0530

    Added DSA11 project files

---
+++ src/Demo11/ClimbingStairs.java
@@ -0,0 +1,21 @@
+package Demo11;
+
+public class ClimbingStairs {
+	static int climbStairs(int n) {
+		if(n <= 2) {
+			return n;
+		}
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		return dp[n];
+	}
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println("Number of ways = " + climbStairs(n));
+	}
+
+}
